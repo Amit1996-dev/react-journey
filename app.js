514c190f7807d2394790833283efc151422d4a5f
Amit1996-dev <amit14243@gmail.com>
2024-12-12 16:26:57 +0530

    Document Object Model

---
+++ app.js
@@ -0,0 +1,12 @@
+// let div = document.querySelector("div").innerText;
+// console.dir(div);
+
+
+// let div = document.querySelector("div").tagName;
+// console.dir(div);
+
+// let div = document.querySelector("div").innerHTML;
+// console.dir(div);
+
+// let div = document.querySelector("div").textContent; //textContent can helps in displayig hidden elements
+// console.dir(div);
